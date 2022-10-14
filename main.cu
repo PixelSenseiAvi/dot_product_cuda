03d96e2e13c409ea20d366980260af164d2d38a1
Avi Garg <a7vi1912@gmail.com>
2022-10-14 13:44:45 +0530

    Update main.cu

--- main.cu
+++ main.cu
@@ -1,80 +1,143 @@
+#include <stdio.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "device_atomic_functions.h"
-#include <stdio.h>
 #include <stdlib.h>
-#define N (2048 * 8)
-#define THREADS_PER_BLOCK 512
 
-__global__ void dot(int *a, int *b, int *c)
+const int N = 1<<24;
+const int threadsPerBlock = 512;
+const int blocksPerGrid = N/threadsPerBlock;
+
+//kernel 1
+__global__ void partial_dot(float* a, float* b, float* c) {
+	__shared__ float cache[threadsPerBlock];
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	int cacheIndex = threadIdx.x;
+	
+	float temp = 0;
+	while (tid < N){
+		temp += a[tid] * b[tid];
+		tid += blockDim.x * gridDim.x;
+	}
+	
+	// set the cache values
+	cache[cacheIndex] = temp;
+	
+	// synchronize threads in this block
+	__syncthreads();
+	
+	// for reductions, threadsPerBlock must be a power of 2
+	// because of the following code
+	int i = blockDim.x/2;
+	while (i != 0){
+		if (cacheIndex < i)
+			cache[cacheIndex] += cache[cacheIndex + i];
+		__syncthreads();
+		i /= 2;
+	}
+	
+	if (cacheIndex == 0)
+		c[blockIdx.x] = cache[0];
+}
+
+//kernel 2
+__global__ void atomic_dot(float *a, float *b, float *c)
 {
-    __shared__ int temp[THREADS_PER_BLOCK];
+    __shared__ float cache[threadsPerBlock];
     int index = threadIdx.x + blockIdx.x * blockDim.x;
-    temp[threadIdx.x] = a[index] * b[index];
+    cache[threadIdx.x] = a[index] * b[index];
 
     __syncthreads();
 
     if (threadIdx.x == 0)
     {
-        int sum = 0;
-        for (int i = 0; i < THREADS_PER_BLOCK; i++)
+        float sum = 0;
+        for (int i = 0; i < threadsPerBlock; i++)
         {
-            sum += temp[i];
+            sum += cache[i];
         }
         atomicAdd(c, sum);
     }
 }
 
-int main()
-{
-    int *a, *b, *c;
-    int *dev_a, *dev_b, *dev_c;
-    int size = N * sizeof(int);
 
-   //allocate space for the variables on the device
-    cudaMalloc((void **)&dev_a, size);
-    cudaMalloc((void **)&dev_b, size);
-    cudaMalloc((void **)&dev_c, sizeof(int));
+int main (void) {
 
-   //allocate space for the variables on the host
-   a = (int *)malloc(size);
-   b = (int *)malloc(size);
-   c = (int *)malloc(sizeof(int));
+  cudaEvent_t start_k1, stop_k1, start_k2, stop_k2;
+  cudaEventCreate(&start_k1); cudaEventCreate(&start_k2);
+  cudaEventCreate(&stop_k1);  cudaEventCreate(&stop_k2);
+  float milliseconds_k1 = 0, milliseconds_k2 = 0;
 
-   //this is our ground truth
-   int sumTest = 0;
-   //generate numbers
-   for (int i = 0; i < N; i++)
-   {
-       a[i] = rand() % 10;
-       b[i] = rand() % 10;
-       sumTest += a[i] * b[i];
-       printf(" %d %d \n",a[i],b[i]);
-   }
+	float *a, *b, c, *t, *partial_c;
+	float *dev_a, *dev_b, *dev_t, *dev_partial_c;
+	
+	// allocate memory on the cpu side
+	a = (float*)malloc(N*sizeof(float));
+	b = (float*)malloc(N*sizeof(float));
+    t = (float *)malloc(sizeof(float));
+	partial_c = (float*)malloc(blocksPerGrid*sizeof(float));
+	
+	// allocate the memory on the gpu
+	cudaMalloc((void**)&dev_a, N*sizeof(float));
+	cudaMalloc((void**)&dev_b, N*sizeof(float));
+    cudaMalloc((void **)&dev_t, sizeof(float));
+	cudaMalloc((void**)&dev_partial_c, blocksPerGrid*sizeof(float));
+	
+    float sumTest = 0;
+	// fill in the host mempory with data
+	for(int i=0; i<N; i++) {
+	    a[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        b[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        sumTest += a[i] * b[i];
+	}
+	printf("sumTest: %f\n",sumTest);
+	
+    *t = 0;
 
-   *c = 0;
+	// copy the arrays 'a' and 'b' to the gpu
+	cudaMemcpy(dev_a, a, N*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, b, N*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_t, t, sizeof(float), cudaMemcpyHostToDevice);
 
-   cudaMemcpy(dev_a, a, size, cudaMemcpyHostToDevice);
-   cudaMemcpy(dev_b, b, size, cudaMemcpyHostToDevice);
-   cudaMemcpy(dev_c, c, sizeof(int), cudaMemcpyHostToDevice);
+    cudaEventRecord(start_k1);
+	partial_dot<<<blocksPerGrid, threadsPerBlock>>>(dev_a, dev_b, dev_partial_c);
+	
+	// copy the array 'c' back from the gpu to the cpu
+	cudaMemcpy(partial_c,dev_partial_c, blocksPerGrid*sizeof(float), cudaMemcpyDeviceToHost);
+	
+	// finish up on the cpu side
+	c = 0;
+	for(int i=0; i<blocksPerGrid; i++) {
+		c += partial_c[i];
+		//printf("parial_c: %.6f \n", partial_c[i]);
+	}
+	cudaDeviceSynchronize();
+    cudaEventRecord(stop_k1);
+    cudaEventElapsedTime(&milliseconds_k1, start_k1, stop_k1);
 
-   dot<<< N / THREADS_PER_BLOCK, THREADS_PER_BLOCK >> >(dev_a, dev_b,    dev_c);
+	#define sum_squares(x) (x*(x+1)*(2*x+1)/6)
+	printf("Kernel 1 value %f ", c);
+    printf("Time elapsed: %f \n", milliseconds_k1);
+	
+    cudaEventRecord(start_k2);
+    atomic_dot<<< blocksPerGrid, threadsPerBlock >>>(dev_a, dev_b, dev_t);
+    cudaDeviceSynchronize();
+    cudaEventRecord(stop_k2);
+    cudaEventElapsedTime(&milliseconds_k2, start_k2, stop_k2);
 
-   cudaMemcpy(c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(t, dev_t, sizeof(float), cudaMemcpyDeviceToHost);
 
-   printf("%d ", *c);
-   printf("%d ", sumTest);
+    printf("Kernel 2: value is %f, total time elapsed %f\n", *t, milliseconds_k2);
 
-   free(a);
-   free(b);
-   free(c);
-
-   cudaFree(a);
-   cudaFree(b);
-   cudaFree(c);
-
-   system("pause");
-
-   return 0;
-
- }
+	// free memory on the gpu side
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFree(dev_partial_c);
+	cudaFree(dev_t);
+	
+	// free memory on the cpu side
+	free(a);
+	free(b);
+	free(partial_c);
+	free(t);
+}
